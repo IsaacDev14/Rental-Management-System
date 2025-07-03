f2da6f4e982349704b9f40f513496346d82dfab4
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-03 09:13:36 +0300

    upadate unit js

---
+++ backend/models/Unit.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UnitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['Single', 'Double', 'Bedsitter', '1 Bedroom', '2 Bedroom'],
+    default: 'Single',
+  },
+  rent: {
+    type: Number,
+    required: true,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  tenantId: {
+    type: String, // You can use ObjectId if referencing Tenant directly
+    default: null,
+  }
+}, { timestamps: true });
+
+module.exports = UnitSchema;
